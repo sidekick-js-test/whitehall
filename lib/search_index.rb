3241ecb7f47d94db504af18ce0bc7079890e7ee2
David Heath <david.heath@digital.cabinet-office.gov.uk>
2013-06-18 17:28:22 +0100

    Retry indexing attempts up to 3 times

--- lib/search_index.rb
+++ lib/search_index.rb
@@ -5,10 +5,20 @@ class SearchIndex
     @name = name
     @rummager_url = rummager_url.sub(%r{/$}, "")
     @logger = options[:logger]
+    @kernel = options[:kernel] || Kernel
+    @retries = options[:retries] || 3
   end
 
   def add(entry)
-    RestClient.post(rummager_endpoint, MultiJson.encode([entry]), content_type: :json, accept: :json)
+    (1..@retries).each do |i|
+      begin
+        RestClient.post(rummager_endpoint, MultiJson.encode([entry]), content_type: :json, accept: :json)
+        return true
+      rescue RestClient::RequestFailed
+        raise if i == @retries
+        @kernel.sleep(5)
+      end
+    end
   end
 
   def add_batch(entries)
--- test/unit/search_index_test.rb
+++ test/unit/search_index_test.rb
@@ -1,26 +1,59 @@
 require 'fast_test_helper'
 
 class SearchIndexTest < ActiveSupport::TestCase
+  def sample_document
+    {link: "/foo"}
+  end
+
   test "can create an index" do
-    index = SearchIndex.new("government")
+    index = SearchIndex.new("government", "http://rummager.test/")
     assert_equal "government", index.name
   end
 
   test "when we add a single document it is posted to rummager" do
-    document = {link: "/foo"}
-    json = MultiJson.encode([document])
-
     stub_request(:post, "http://rummager.test/government/documents").
       to_return(status: 200, body: '{"status":"OK"}')
 
     index = SearchIndex.new("government", "http://rummager.test/")
-    index.add(document)
+    index.add(sample_document)
 
     assert_requested :post, "http://rummager.test/government/documents" do |request|
-      request.body == json &&
+      request.body == MultiJson.encode([sample_document]) &&
         request.headers['Content-Type'] == 'application/json' &&
         request.headers['Accept'] == 'application/json'
     end
   end
 
+  test "returns true on success" do
+    stub_request(:post, "http://rummager.test/government/documents").
+      to_return(status: 200, body: '{"status":"OK"}')
+
+    index = SearchIndex.new("government", "http://rummager.test/")
+    assert index.add(sample_document)
+  end
+
+  test "retries on 502 bad gateway error" do
+    stub_request(:post, "http://rummager.test/government/documents").
+      to_return(status: 502, body: 'Bad gateway').times(1).then.
+      to_return(status: 200, body: '{"status":"OK"}')
+
+    kernel = stub("kernel")
+    kernel.expects(:sleep).with(5)
+
+    index = SearchIndex.new("government", "http://rummager.test/", kernel: kernel)
+    assert index.add({link: "/foo"})
+  end
+
+  test "propogates any exception after too many failed attempts" do
+    stub_request(:post, "http://rummager.test/government/documents").
+      to_return(status: 502, body: 'Bad gateway').times(4)
+
+    kernel = stub("kernel")
+    kernel.expects(:sleep).with(5).at_least_once
+
+    index = SearchIndex.new("government", "http://rummager.test/", kernel: kernel)
+    assert_raises RestClient::BadGateway do
+      index.add({link: "/foo"})
+    end
+  end
 end
